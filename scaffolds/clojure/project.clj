6dd459770fca760169946f137e832701d88468ef
Jehan Bruggeman <jbruggem@users.noreply.github.com>
2016-06-10 14:23:52 +0200

    Clojure: fixes to project.clj

--- scaffolds/clojure/project.clj
+++ scaffolds/clojure/project.clj
@@ -1,4 +1,4 @@
 (defproject code-retreat-scaffold-clj "0.1.0-SNAPSHOT"
   :description "Clojure code retreat scaffold"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main "scaffold.core")
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main scaffold.core)
