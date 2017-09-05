23876ab1a624432d1146b0edc1aae86ffa7ad6d2
Julien Poulin <julien.poulin@euranova.eu>
2017-09-05 14:02:12 +0200

    Update bash script to use the .NET Core 2.0.0 image

--- scaffolds/csharp/docker.sh
+++ scaffolds/csharp/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v $PWD:/app -w /app/src/CodeRetreat microsoft/dotnet:1.1-sdk-projectjson bash
+docker run -it --rm -v ${PWD}:/app -w /app/CodeRetreat.Tests microsoft/dotnet:2.0.0-sdk bash
