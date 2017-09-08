741aa077b57727df1e493737e4500364c9b07a58
Julien Poulin <julien.poulin@euranova.eu>
2017-09-08 10:01:21 +0200

    Add a $@ at the end of the line so that arguments are passed to the bash command

--- scaffolds/csharp/docker.sh
+++ scaffolds/csharp/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v ${PWD}:/app -w /app/CodeRetreat.Tests microsoft/dotnet:2.0.0-sdk bash
+docker run -it --rm -v ${PWD}:/app -w /app/CodeRetreat.Tests microsoft/dotnet:2.0.0-sdk bash $@
