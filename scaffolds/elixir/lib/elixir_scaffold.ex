103097bd1346bdf014d6a5cb1cf4d40550dc4a12
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2018-11-23 09:27:15 +0100

    Add elixir scaffold

---
+++ scaffolds/elixir/lib/elixir_scaffold.ex
@@ -0,0 +1,18 @@
+defmodule ElixirScaffold do
+  @moduledoc """
+  Documentation for ElixirScaffold.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirScaffold.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
